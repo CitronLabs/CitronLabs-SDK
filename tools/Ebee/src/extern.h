7f13bc412f58f4f64e9ae1e215690983dc470534
blueberry <blueberryprograms@gmail.com>
2025-07-23 20:19:18 -0400

    started work on ebee toolchain

---
+++ tools/Ebee/src/extern.h
@@ -0,0 +1,3 @@
+#include "../../../libs/APIs/XC/core.h"
+#include "../../../libs/APIs/XC/resource.h"
+#include "../../../libs/APIs/XC/os.h"
--- tools/Ebee/src/main.c
+++ tools/Ebee/src/main.c
@@ -1,8 +1,37 @@
-#include "std-all.h"
+#include "extern.h"
+#include "types.h"
+
+Type(EbeeFlags,
+	u32 verbose : 1;
+)
+
+Data(EbeeArgs,
+__INIT(cstr* v),
+	EbeeFlags flags;
+){
+	for(int i = 0;;i++){
+		if(args.v[i] == NULL) break;
+		if(args.v[i][0] == '-'); 
+	}
+return self;
+}
+
+Class(EbeeTool,
+__INIT(inst(EbeeArgs) args),
+__FIELD(
+	inst(MemoryMap) memory;
+      	inst(EbeeFlags) flags;
+      	
+),	
+      	interface(Loggable);
+      	errvt method(EbeeTool, createEnv,, fsPath path);
+)
+
 
 int main(int argc, char **argv){
+	
+	inst(EbeeTool) ebee = push(EbeeTool, push(EbeeArgs, argv));
 
-	println("Hello World!");
 
 return 0;
 }
