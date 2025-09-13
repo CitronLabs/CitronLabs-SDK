2c587934e3bf5e233b6d683b266128eae05393e0
blueberry <blueberryprograms@gmail.com>
2025-09-13 19:58:12 -0400

    xc-os-linux: added include statements to API header

--- libs/APIs/XC/builtin/os/linux.h
+++ libs/APIs/XC/builtin/os/linux.h
@@ -9,6 +9,16 @@
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 #include <semaphore.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <sys/epoll.h>
 
 Static(LinuxEnv,
 	interface(OS);
